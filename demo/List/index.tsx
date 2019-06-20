3cb5d50fc0a2e79e22952c80ab7996823c4ef387
zhangjiayong <zhangjiayong@zuzuche.com>
2019-06-20 20:31:51 +0800

    完成 List 组件的demo

--- demo/List/index.tsx
+++ demo/List/index.tsx
@@ -1,6 +1,8 @@
 
 import * as React from 'react';
+import List from '../../packages/List';
 import './style.scss';
+
 
 export interface Props {
 
@@ -11,6 +13,35 @@ export default function ListPage() {
   return (
     <div id="list-page">
       <h1>List</h1>
+      <div>
+        <List
+          header="我是 header"
+          footer="我是 footer"
+        >
+          <List.Item>list-1</List.Item>
+          <List.Item>list-2</List.Item>
+          <List.Item>list-3</List.Item>
+        </List>
+
+        <List
+          header={(<strong>可点击</strong>)}
+        >
+          <List.Item
+            onClick={() => {
+              alert(1);
+            }}
+          >
+            alert(1)
+          </List.Item>
+          <List.Item
+            onClick={() => {
+              alert(2);
+            }}
+          >
+            alert(2)
+          </List.Item>
+        </List>
+      </div>
     </div>
   );
 }
